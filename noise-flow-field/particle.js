98c5a25ebd5a8cbaa094c540558fba7a8f18d7a3
Nico <klaassen.nico@gmail.com>
2020-04-29 00:54:21 +0200

    start on noise flow field

---
+++ noise-flow-field/particle.js
@@ -0,0 +1,28 @@
+class Particle {
+    constructor(x, y, psize) {
+        this.psize  = psize;
+
+        this.pos = createVector(x, y);
+        this.vel = createVector(0, 0); 
+        this.acc = createVector(0, 0);
+    }
+
+    add_force(force) {
+        this.acc.add(force);
+    }
+
+    update() {
+        this.vel.add(this.acc);
+        this.vel.normalize();
+        this.pos.add(this.vel);
+        this.acc.set(0, 0);
+        
+        this.pos.x = (this.pos.x + width) % width;
+        this.pos.y = (this.pos.y + height) % height;
+    }
+
+
+    render() {
+        circle(this.pos.x, this.pos.y, this.psize);
+    }
+}
---
+++ noise-flow-field/sketch.js
@@ -0,0 +1,51 @@
+let particles;
+let iteration;
+
+function setup() {
+    createCanvas(1920, 1080);
+    iteration = 0;
+    noStroke();
+    background(0);
+}
+
+function draw() {
+    //if (iteration == 0) {
+    //    generate_particles(1000, 30, 255);
+    //} else if (iteration == 100) {
+    //    generate_particles(1000, 20, 200);
+    //} else if (iteration == 200) {
+    //    generate_particles(1000, 10, 150);
+    //} else if (iteration == 300) {
+    //    generate_particles(2000, 5, 100);
+    //} else if (iteration == 400) {
+    //    generate_particles(3000, 3, 50);
+    //} else if (iteration == 500) {
+    //    generate_particles(5000, 1, 0);
+    //}
+    if (iteration == 0) {
+        generate_particles(1000, 2, 255);
+    }
+    update_particles();
+    iteration++;
+    console.log(iteration);
+}
+
+function generate_particles(count, psize, pcolor) {
+    particles = [];
+    fill(pcolor, pcolor, pcolor, 25);
+    for (let i = 0; i < count; i++) {
+        let x = random(width);
+        let y = random(height);
+        particles.push(new Particle(x, y, psize));
+    }
+}
+
+function update_particles() {
+    for (let p of particles) {
+        let angle = noise(p.pos.x, p.pos.y) * 8 * PI;
+        let noise_field_force = p5.Vector.fromAngle(angle);
+        p.add_force(noise_field_force);
+        p.update();
+        p.render();
+    }
+}

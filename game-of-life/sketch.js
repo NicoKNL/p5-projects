f440fedd705561c523c4de247ecda3b034dd5ac6
Nico <klaassen.nico@gmail.com>
2020-05-19 10:24:31 +0200

    small refactor

--- game-of-life/sketch.js
+++ game-of-life/sketch.js
@@ -3,9 +3,11 @@ let columns;
 let rows;
 let board;
 let next;
-
+let c;
+let current_frame;
 function setup() {
-    createCanvas(512, 512);
+    c = createCanvas(512, 512);
+    current_frame = 0;
     frameRate(6);
     noStroke();
 
@@ -36,6 +38,9 @@ function draw() {
             rect(col * cell_size, row * cell_size, cell_size - 1, cell_size - 1);
         }
     }
+    let filename = `${current_frame}`
+    saveCanvas(c, "frame_" + filename.padStart(3, "0") + ".png");
+    current_frame += 1;
     update();
 }
 
@@ -51,7 +56,7 @@ function initRandom() {
 function update() {
     for (let row = 0; row < rows; row++) {
         for (let col = 0; col < columns; col++) {
-            let neighbors = getNeighbours(row, col);
+            let neighbors = getNeighbors(row, col);
             let count = 0;
             for (let i = 0; i < neighbors.length; i++) {
                 let x = neighbors[i][0];
@@ -74,13 +79,13 @@ function update() {
     }
 }
 
-function getNeighbours(row, col) {
-    let neighbours = [];
+function getNeighbors(row, col) {
+    let neighbors = [];
     for (let y = row - 1; y <= row + 1; y++) {
         for (let x = col - 1; x <= col + 1; x++) {
             if (x === col && y === row) continue;
-            neighbours.push([(x + columns) % columns, (y + rows) % rows])
+            neighbors.push([(x + columns) % columns, (y + rows) % rows])
         }
     }
-    return neighbours
+    return neighbors
 }
